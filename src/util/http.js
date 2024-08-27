42d1b6d4eb8f998debe43a25dd48b8015a8e4a3a
MiaChensSV <mia.chen@dhanticounterfeit.com>
2024-08-27 08:12:31 +0200

    change calendar api return max value

--- src/util/http.js
+++ src/util/http.js
@@ -1,34 +1,39 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_CALENDAR_URL + process.env.REACT_APP_CALENDAR_ID   + '/events'
+const BASE_URL =
+  process.env.REACT_APP_CALENDAR_URL +
+  process.env.REACT_APP_CALENDAR_ID +
+  "/events";
 
 // Function to fetch events from the Google Calendar
 const fetchEvents = async () => {
-  return new Promise((resolve) => {
-    try {
-      axios
-        .get(BASE_URL, {
-          params: {
-            key: process.env.REACT_APP_API_KEY,
-            timeMin: new Date().toISOString(),
-            maxResults: 10,
-            singleEvents: true,
-            orderBy: "startTime",
-          },
-        })
-        .then((response) => {
-          if (response.data.items.length) {
-            const eventArray = response.data.items.map((event) => ({
-              startDate: event.start.dateTime || event.start.date,
-              endDate: event.end.dateTime || event.end.date,
-              summary: event.summary,
-            }));
-            resolve(eventArray);
-          }else resolve([])
-        });
-    } catch (error) {
-      console.error("Error fetching events:", error);
-    }
-  });
+  let allEvents = [];
+  let pageToken = null;
+  try {
+    do {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          key: process.env.REACT_APP_API_KEY,
+          timeMin: new Date().toISOString(),
+          singleEvents: true,
+          orderBy: "startTime",
+          maxResults: 2500,
+          pageToken: pageToken,
+        },
+      });
+      const events = response.data.items.map((event) => ({
+        startDate: event.start.dateTime || event.start.date,
+        endDate: event.end.dateTime || event.end.date,
+        summary: event.summary,
+      }));
+      allEvents = allEvents.concat(events);
+      // Update the pageToken to get the next page of results
+      pageToken = response.data.nextPageToken;
+    } while (pageToken);
+    return allEvents;
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
 };
 export default fetchEvents;
 
