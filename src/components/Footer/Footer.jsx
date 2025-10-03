437681f76cdce127fff65b980599d47a94f5ef5d
Mia Chen <miachensv@gmail.com>
2025-10-03 11:11:06 +0200

    change Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -41,8 +41,8 @@ const Footer = () => {
             <h4>Contact US</h4>
             <div className="contact">
               <p className="title">Email: </p>
-              <a href="mailto:info@vacationnerja.com?cc=miachensv@gmail.com&subject=Request%20for%20Casa%20Mia&body=I%20would%20like%20to%20ask%20about%20the%20availibility%20of%20apartment%20of%20Casa%20Mia%20in%20Nerja:">
-                info@vacationnerja.com
+              <a href="mailto:casamiaguests@gmail.com?cc=miachensv@gmail.com&subject=Request%20for%20Casa%20Mia&body=I%20would%20like%20to%20ask%20about%20the%20availibility%20of%20apartment%20of%20Casa%20Mia%20in%20Nerja:">
+                casamiaguests@gmail.com
               </a>
             </div>
             <div className="contact">
